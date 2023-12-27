fee701eb1e604b0a322d97c8d5140ee614c75d90
Yurii <shadowjura82@gmail.com>
2023-12-27 16:31:46 +0200

    Initial commit

---
+++ src/main/java/com/iTMS/iTMS/ITmsApplication.java
@@ -0,0 +1,13 @@
+package com.iTMS.iTMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ITmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ITmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iTMS/iTMS/ITmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iTMS.iTMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ITmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
