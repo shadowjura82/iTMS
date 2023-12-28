a4e3fefc5da91b050b7659f20de93dc3f41aea90
Yurii <shadowjura82@gmail.com>
2023-12-28 10:54:51 +0200

    Swagger has been connected

--- src/main/java/com/iTMS/iTMS/controllers/TstController.java
+++ src/main/java/com/iTMS/iTMS/controllers/TstController.java
@@ -1,10 +1,13 @@
 package com.iTMS.iTMS.controllers;
 
 import com.iTMS.iTMS.services.TstService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "API для iTMS бота")
 public class TstController {
 
     private final TstService tstService;
@@ -14,6 +17,7 @@ public class TstController {
     }
 
     @GetMapping
+    @Operation(summary = "Это тестовый ендпоинт")
     public String test() {
         return "Test message";
     }
