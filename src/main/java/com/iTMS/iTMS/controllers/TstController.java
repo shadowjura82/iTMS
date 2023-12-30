7428b686f61db55108c24af48f15e8951e1c71e5
shadowjura82 <shadowjura82@gmail.com>
2023-12-30 09:47:35 +0100

    Jenkins

--- src/main/java/com/iTMS/iTMS/controllers/TstController.java
+++ src/main/java/com/iTMS/iTMS/controllers/TstController.java
@@ -1,16 +1,19 @@
 package com.iTMS.iTMS.controllers;
 
+import com.iTMS.iTMS.WebExlib.Message;
+import com.iTMS.iTMS.WebExlib.Webhook;
 import com.iTMS.iTMS.services.TstService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Tag(name = "API для iTMS бота")
-public class TstController {
+public class  TstController {
 
     private final TstService tstService;
 
@@ -24,9 +27,9 @@ public class TstController {
         return tstService.getToken();
     }
 
-    @PostMapping
+    @PostMapping("/webhook")
     @Operation(summary = "Тестовый webhook")
-    public void webhook() {
+    public void webhook(@RequestBody Webhook message) {
         System.out.println("Метод отработал");
     }
 }
