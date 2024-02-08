9148094b139532c23525c711ff6d2cbccd745c4e
Yurii <shadowjura82@gmail.com>
2024-02-08 23:45:03 +0200

    Добавил контроллер для утилит и утилиту для подсчета времени потраченого ПЛД командой на таски с нашей помошью и без. Пока без реализации

--- src/main/java/com/iTMS/iTMS/controllers/UtilitiesController.java
+++ src/main/java/com/iTMS/iTMS/controllers/UtilitiesController.java
@@ -1,4 +1,33 @@
 package com.iTMS.iTMS.controllers;
 
+import com.iTMS.iTMS.dto.TaskId;
+import com.iTMS.iTMS.utilities.CheckPLDtimeSpend;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@Tag(name = "API для работы с утилитами")
+@RequestMapping("/utilities")
 public class UtilitiesController {
+    @Autowired
+    private CheckPLDtimeSpend checkPLDtimeSpend;
+
+    @GetMapping("/check_pld_time_spend")
+    @Operation(summary = "Среднее арифметическое время потраченое командой PLD на обработку задач",
+            description = "Этот ендпоинт позволяет сравнить время потраченное на рефение задач без помощи и с помощью сапорта<br>" +
+                    "Параметры:<br>" +
+                    "<b>dateFrom</b> - Начальная дата в формате YYYY-MM-DD HH24:MI:SS<br>" +
+                    "<b>dateTo</b> - Окончательная дата в формате YYYY-MM-DD HH24:MI:SS<br>" +
+                    "<b>RequestBody</b> - Список тасок в формате TaskId объектов")
+    public ResponseEntity<String> getTasks(@RequestParam(defaultValue = "2024-01-01 00:00:00") String dateFrom,
+                                           @RequestParam(defaultValue = "2025-01-01 23:59:00") String dateTo,
+                                           @RequestParam(required = false, defaultValue = "C") String status,
+                                           @RequestBody TaskId taskId) {
+        return ResponseEntity.ok(checkPLDtimeSpend.getComparison(dateFrom, dateTo, status, taskId));
+    }
 }
--- src/main/java/com/iTMS/iTMS/utilities/CheckPLDtimeSpend.java
+++ src/main/java/com/iTMS/iTMS/utilities/CheckPLDtimeSpend.java
@@ -1,8 +1,11 @@
 package com.iTMS.iTMS.utilities;
 
+import com.iTMS.iTMS.dto.TaskId;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CheckPLDtimeSpend {
-
+    public String getComparison(String dateFrom, String dateTo, String status, TaskId taskId) {
+        return null;
+    }
 }
